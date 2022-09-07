e1f6446061fd826fdb74c3c6683f7bb1ba2f053a
Kristoffer Snopestad Søderkvist <55276856+Bass4Nation@users.noreply.github.com>
2022-09-07 15:17:53 +0200

    loader

---
+++ components/Frontpage.tsx
@@ -0,0 +1,24 @@
+import { useState, CSSProperties } from "react";
+import PacmanLoader from "react-spinners/PacmanLoader";
+
+const override: CSSProperties = {
+  display: "block",
+  margin: "0 auto",
+  borderColor: "red",
+};
+
+function Frontpage() {
+  let [loading, setLoading] = useState(true);
+  let [color, setColor] = useState("#666666");
+
+  return (
+    <div className="sweet-loading">
+      {/* <button onClick={() => setLoading(!loading)}>Toggle Loader</button> */}
+      {/* <input value={color} onChange={(input) => setColor(input.target.value)} placeholder="Color of the loader" /> */}
+
+      <PacmanLoader color={color} loading={loading} cssOverride={override} size={150} />
+    </div>
+  );
+}
+
+export default Frontpage;
