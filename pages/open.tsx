ff86cedb14687a2a83268fa28fbc5ed7c31d0f23
Bass4Nation <55276856+Bass4Nation@users.noreply.github.com>
2022-09-13 12:58:27 +0200

    Mobil test1

--- pages/open.tsx
+++ pages/open.tsx
@@ -1,4 +1,6 @@
-// import { useState, CSSProperties } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { useState, CSSProperties } from "react";
 // import PacmanLoader from "react-spinners/PacmanLoader";
 
 // const override: CSSProperties = {
@@ -9,7 +11,7 @@
 
 // Simulere 2 sekunder lasting
 export async function getStaticProps() {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   return {
     props: {},
   };
@@ -21,10 +23,24 @@ function Open() {
   //   let [color, setColor] = useState("#666666");
   // Create your instance
 
+
+  // tanker.
+  // If screen size is mobile use .mobile css eller if desktop use .desktop
+  // Trenger bare å endre hvem css som skal bli brukt. Eks en useState 
+
+  // mobile / desktop = 
+  let [display, setDisplay] = useState("mobile")
+  let [backgroundImg, setBackgroundImg] = useState("/fold/mobile.png")
+
   return (
     <>
-      <section>
-        <h1>Hi, and welcome to my portofolio page</h1>
+      <section className={display}>
+        <Image src={backgroundImg} layout="fill" className="phone"/>
+        <h1>KSS Portofolio side</h1>
+        <section>
+          <Link href="/">Om meg</Link>
+        </section>
+        <p>Dette er en side for å vise noe av hva jeg kan. </p>
       </section>
     </>
   );
